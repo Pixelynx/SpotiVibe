86a0958024495bb6d050f95c18fb2a7e9e701e90
pixelynx <tjstevens1992@gmail.com>
2024-07-18 16:25:01 -0400

    setup server

--- spotify_api.py
+++ spotify_api.py
@@ -1,5 +1,17 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_spotify_client():
+    return spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        scope="user-library-read"
+    ))
 
 def authenticate():
     client_credentials_manager = SpotifyClientCredentials(client_id='YOUR_CLIENT_ID', client_secret='YOUR_CLIENT_SECRET')
